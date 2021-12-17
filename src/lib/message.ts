ffc6103ff5eae8e27c54e96333c9783b3227a752
susheng <2771885247@qq.com>
2021-12-17 10:29:35 +0800

    feat(#9): @lib promiseConfirm增加catch回调函数

--- src/lib/message.ts
+++ src/lib/message.ts
@@ -9,6 +9,7 @@ import { Modal } from "ant-design-vue";
  * 调用方式-参数调用，OK的loading会等待promiseMethod执行后关闭。promiseMethod出现异常时，会关闭ok-loading
  * @param content 主题内容
  * @param promiseMethod ok时执行的异步函数
+ * @param catchMethod promiseMethod异常回调
  * @param opt Modal-confirm 配置项
  * @returns void
  */
@@ -19,6 +20,7 @@ export const promiseConfirm = (
     okText: () => "确定",
     cancelText: () => "取消",
   },
+  catchMethod?: () => void,
 ) => {
   const modal = Modal.confirm({
     ...opt,
@@ -32,6 +34,7 @@ export const promiseConfirm = (
           .catch(() => {
             reject();
             modal.destroy();
+            if (catchMethod) catchMethod();
           });
       }),
   });
