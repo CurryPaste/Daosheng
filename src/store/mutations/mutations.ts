b4b271c47c18eb05678b8a94b346e6616811025f
susheng <2771885247@qq.com>
2022-02-14 17:22:30 +0800

    remove(#9): ➖ 去除多余代码，保证单仓库正常

--- src/store/mutations/mutations.ts
+++ src/store/mutations/mutations.ts
@@ -1,7 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { MutationTree } from "vuex";
 import MutationTypes from "./mutation-types";
 import { State } from "../states";
-/* eslint-disable no-unused-vars */
 
 export type Mutations<S = State> = {
   [MutationTypes.SET_TITLE](state: S, payload: string): void;
